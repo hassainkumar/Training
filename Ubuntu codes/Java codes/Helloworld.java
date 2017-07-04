fad9203b2137e44bdd5c1631739c821ffef977f4
Ubuntu <hassainkumar1993@gmail.com>
2017-07-04 16:20:02 -0500

    My java codes are stated

---
+++ Ubuntu codes/Java codes/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi this is my first programe in jave with out refering");
+	}
+}
---
+++ Ubuntu codes/Java codes/java.sh
@@ -0,0 +1,6 @@
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+#gedit  /etc/profile
+#add this in this line at end
+#export JAVA_HOME=/usr/lib/jvm/java-8-oracle
